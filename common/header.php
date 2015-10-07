9acc67fe6d06e609c7f3f0accd71e14e50392e8d
Tran TRI DUE <dtrantri@pentalog.fr>
2015-10-07 09:35:08 +0700

    a

--- common/header.php
+++ common/header.php
@@ -1,11 +1,15 @@
 <?php
-//$users = $userService->getAllUser ();
-//for($i = 0; $i < count ( $users ); $i ++) {
-//	echo "<br>" . $users [$i] ['id'].". " . $users [$i] ['name'];
-//}
+/*
+$users = $userService->getAllUser ();
+for($i = 0; $i < count ( $users ); $i ++) {
+	echo "<br>" . $users [$i] ['id'].". " . $users [$i] ['name'];
+} 
+*/
+
 $flag = true;
 mysql_query ( "BEGIN" );
 
+/*
 $table = "category";
 $colums = array ('id', 'name' );
 $values = array ('2', 'due');
@@ -18,6 +22,9 @@ $values = array ('2', 'due');
 
 
 $flag = $flag && $commonUtil->addRow ( $table, $colums, $values );
+*/
+
+
 
 $commonUtil->commitTransaction($flag)
 ?>

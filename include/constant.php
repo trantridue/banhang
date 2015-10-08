abc6888e3253c61b420526587a69b002243bc665
Tran TRI DUE <dtrantri@pentalog.fr>
2015-10-08 20:49:19 +0700

    a

--- include/constant.php
+++ include/constant.php
@@ -8,6 +8,7 @@ define('username', 'zabuzachaule');
 define('password', 'canhd0ngh0ang1');
 define('database', 'banhang');
 define('tablename', 'user');
+
 define('number_column_config_form', 4);
 
 

5cdea3cfead683c1ac15f5c073d6ef1c3eb8e958
Tran TRI DUE <dtrantri@pentalog.fr>
2015-10-08 09:12:49 +0700

    a

--- include/ConfigService.php
+++ include/ConfigService.php
@@ -1,18 +1,27 @@
 <?php
 class ConfigService {
 	var $commonUtil;
-	
-	function ConfigService($commonUtil) {
-		$this->commonUtil = $commonUtil;
+	var $qry = "select * from configuration";
+
+function ConfigService($commonUtil) {
+	$this->commonUtil = $commonUtil;
+}
+//	load all configuration parameters into session
+function loadConfiguration() {
+	session_start ();
+	$result = $this->commonUtil->getResultByQuery ( $this->qry );
+	for($i = 0; $i < count ( $result ); $i ++) {
+		$_SESSION [$result [$i] ['name']] = $result [$i] ['value'];
 	}
-	
-	function loadConfiguration() {
-		session_start ();
-		$qry = "select * from configuration";
-		$result = $this->commonUtil->getResultByQuery ( $qry );
-		for($i = 0; $i < count ( $result ); $i ++) {
-			$_SESSION [$result [$i] ['name']] = $result [$i] ['value'];
-		}
+}
+
+function loadConfiguration() {
+	session_start ();
+	$result = $this->commonUtil->getResultByQuery ( $this->qry );
+	for($i = 0; $i < count ( $result ); $i ++) {
+		$_SESSION [$result [$i] ['name']] = $result [$i] ['value'];
 	}
+}
+
 }
 ?>

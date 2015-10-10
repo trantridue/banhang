da3bf20423286d661a12b1d95c27dbafe08def3d
Tran TRI DUE <dtrantri@pentalog.fr>
2015-10-10 09:26:21 +0700

    a

--- common/body.php
+++ common/body.php
@@ -2,3 +2,10 @@
 //echo $configService->generateConfigurationEditForm ();
 //echo $_SESSION [prefix_session_config . 'nbr_row_import'];
 ?>
+<div id="body_btn"><?php
+include 'common/body_btn.php';
+?> </div>
+<hr>
+<div id="body_data"><?php
+include 'common/body_data.php';
+?> </div>
